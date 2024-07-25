e36f9a3a5ab4182823155ab17036b3bd6dc887d5
URR505 <mizaelmizrrainub@gmail.com>
2024-07-24 23:36:10 -0600

    CORS

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-
 import express from 'express';
 import hotelsRoutes from './routes/hotels.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import cookieParser from 'cookie-parser';
-
 import cors from 'cors';
 
 const app = express();
 
-// Configuración de CORS para permitir todos los orígenes
-app.use(cors());
+// Configuración de CORS para permitir solicitudes solo desde una URL específica
+const corsOptions = {
+  origin: 'https://hotel-frontend-kmfh.onrender.com',
+  optionsSuccessStatus: 200 // Para navegadores antiguos o ciertos clientes
+};
+
+app.use(cors(corsOptions));
 
 // Middleware para parsear las peticiones como JSON
 app.use(express.json());
 app.use(cookieParser());
+
 // Rutas
 app.use(hotelsRoutes);
 app.use(authRoutes);
 
-
-
 export default app;
-
